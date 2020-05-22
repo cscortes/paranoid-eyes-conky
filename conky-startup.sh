8b1a8405375e0abe69cbd195f3bd3e245797e6ef
Luis Cortes <cscortes@users.noreply.github.com>
2020-05-22 01:13:42 -0600

    added working "start" routine.

--- conky-startup.sh
+++ conky-startup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # conky -c ~/.conky/right_conkyrc &
-# conky -c ~/.conky/left_conkyrc &
+conky -c ~/.conky/DATA/right_summary &
+conky -c ~/.conky/DATA/right_battery &
+conky -c ~/.conky/DATA/right_memory &
+conky -c ~/.conky/DATA/right_cpu &
+conky -c ~/.conky/DATA/right_hd &
+conky -c ~/.conky/DATA/left_conkyrc &
