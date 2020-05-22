4cf83c51d5a6d085bf5af2bf080b21c6ffd38c7b
Luis Cortes <cscortes@users.noreply.github.com>
2020-05-22 10:32:11 -0600

    minor tweaks

--- conky-startup.sh
+++ conky-startup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # conky -c ~/.conky/right_conkyrc &
+sleep 4
 conky -c ~/.conky/DATA/right_summary &
+sleep 1
 conky -c ~/.conky/DATA/right_battery &
+sleep 1
 conky -c ~/.conky/DATA/right_memory &
+sleep 1
 conky -c ~/.conky/DATA/right_cpu &
+sleep 1
 conky -c ~/.conky/DATA/right_hd &
+sleep 6
 conky -c ~/.conky/DATA/left_conkyrc &
