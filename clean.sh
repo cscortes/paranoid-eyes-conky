d23666877e8212bc4b8de946fd4bd5f24441529d
Luis Cortes <cscortes@users.noreply.github.com>
2020-05-31 21:31:50 -0600

    cleanup for config files

---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+killall -9 conky
+rm ./DATA/right_*
+rm ./DATA/left_*
+
--- conky-startup.sh
+++ conky-startup.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 
-# --------------------------------------------------
-#
 ./bin/make_left.py ./DATA/LEFT/config ./DATA/LEFT/left_devconn      ./DATA/left_devconn 
 ./bin/make_left.py ./DATA/LEFT/config ./DATA/LEFT/left_netdev       ./DATA/left_netdev 
 ./bin/make_left.py ./DATA/LEFT/config ./DATA/LEFT/left_netapscan    ./DATA/left_netapscan 
 ./bin/make_left.py ./DATA/LEFT/config ./DATA/LEFT/left_inconn       ./DATA/left_inconn 
 
-conky -c ./DATA/left_devconn &
-conky -c ./DATA/left_netdev &
-conky -c ./DATA/left_netapscan &
-conky -c ./DATA/left_inconn &
+./bin/make_left.py ./DATA/LEFT/config ./DATA/RIGHT/right_summary    ./DATA/right_summary
+./bin/make_left.py ./DATA/LEFT/config ./DATA/RIGHT/right_battery    ./DATA/right_battery 
+./bin/make_left.py ./DATA/LEFT/config ./DATA/RIGHT/right_memory     ./DATA/right_memory
+./bin/make_left.py ./DATA/LEFT/config ./DATA/RIGHT/right_cpu        ./DATA/right_cpu 
+./bin/make_left.py ./DATA/LEFT/config ./DATA/RIGHT/right_hd         ./DATA/right_hd
 
-./bin/make_left.py ./DATA/LEFT/config ./DATA/RIGHT/right_summary ./DATA/right_summary
-./bin/make_left.py ./DATA/LEFT/config ./DATA/RIGHT/right_battery ./DATA/right_battery 
-./bin/make_left.py ./DATA/LEFT/config ./DATA/RIGHT/right_memory  ./DATA/right_memory
-./bin/make_left.py ./DATA/LEFT/config ./DATA/RIGHT/right_cpu     ./DATA/right_cpu 
-./bin/make_left.py ./DATA/LEFT/config ./DATA/RIGHT/right_hd      ./DATA/right_hd
-
-sleep 5
-conky -c ./DATA/right_summary &
-sleep 5
-conky -c ./DATA/right_battery &
-sleep 5
-conky -c ./DATA/right_memory &
-sleep 5
-conky -c ./DATA/right_cpu &
-sleep 5
-conky -c ./DATA/right_hd &
+sleep 2 && conky -c ./DATA/left_devconn &
+sleep 2 && conky -c ./DATA/left_netdev &
+sleep 2 && conky -c ./DATA/left_netapscan &
+sleep 2 && conky -c ./DATA/left_inconn &
+sleep 2 && conky -c ./DATA/right_summary &
+sleep 2 && conky -c ./DATA/right_battery &
+sleep 2 && conky -c ./DATA/right_memory &
+sleep 2 && conky -c ./DATA/right_cpu &
+sleep 2 && conky -c ./DATA/right_hd &
